c43f6b8017a1a7a1fb1f5c98de04a02b86bfc84c
Manu <malonsorodriguez@danielcastelao.org>
2016-12-09 09:49:54 +0000

    404 corregido

--- 404.php
+++ 404.php
@@ -4,7 +4,9 @@
         <link rel="stylesheet" href="wp-content/themes/TemaPropio/style.css" type="text/css" />
     </head>
     <body>
-        <h1>ERRORSITO 404</h1>
-        <img src="<?php echo get_template_directory_uri()?>/images/imagen.gif"/>
+        <div id="error404">
+            <h1>ERRORSITO 404</h1>
+            <img src="<?php echo get_template_directory_uri()?>/images/imagen.gif"/>
+        </div>
     </body>
 </html>
